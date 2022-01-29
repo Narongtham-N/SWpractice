17632a1d2cb8a26e4bc74d723b6612389d9a5525
Narongtham <envnom.b@gmail.com>
2022-01-30 03:53:37 +0700

    Connect with MogoDB Atlas

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //route file
 const hospitals = require('./routes/hospitals');
@@ -7,10 +8,24 @@ const hospitals = require('./routes/hospitals');
 //load env vars
 dotenv.config({path:'./config/config.env'});
 
+//connect to database
+connectDB();
+
 const app = express();
 
+//body parser
+app.use(express.json());
 
 app.use('/api/v1/hospitals', hospitals)
 
 const PORT = process.env.PORT;
-app.listen(PORT, console.log('Server is running in', process.env.NODE_ENV,'mode on port', PORT));
+
+const server = app.listen(PORT, console.log('Server is running in', process.env.NODE_ENV,'mode on port', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error: ${err.message}`);
+
+    //close server and exit process
+    server.close(() => {process.exit(1)});
+})
