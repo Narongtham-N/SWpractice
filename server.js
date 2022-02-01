b4bb5100ac1345240779520e76cf9b402f4696b7
Narongtham <envnom.b@gmail.com>
2022-02-01 16:25:55 +0700

    authentication and authorization

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
 
 //route file
 const hospitals = require('./routes/hospitals');
+const auth =require('./routes/auth');
 
 //load env vars
 dotenv.config({path:'./config/config.env'});
@@ -16,7 +18,11 @@ const app = express();
 //body parser
 app.use(express.json());
 
-app.use('/api/v1/hospitals', hospitals)
+//cookie parser
+app.use(cookieParser());
+
+app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT;
 
